f758f188f0e370fee109917151adb24b3bec565b
liziq21 <lzqlzq21@gmail.com>
2025-11-24 15:05:10 +0800

    update

--- lib/network/model/search/network_search.dart
+++ lib/network/model/search/network_search.dart
@@ -45,7 +45,7 @@ Map<SearchResultType, List<NetworkSearchResult>?> _resultMapFromJson(Object json
       if ((json[0] as Map<String, dynamic>).containsKey('result_type')) {
         return {
           for (var entry in json)
-            (entry as Map<String, dynamic>)['data'] as Map<String, dynamic> dataMap:
+            ((entry as Map<String, dynamic>)['data'] as Map<String, dynamic>) dataMap:
               SearchResultType.parse(dataMap['type']): _resultsFromJson(dataMap),
         };
       // 其它类型搜索结果
